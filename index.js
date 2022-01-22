13cf145485d26f26dbbd194eed7bd4b496dacd46
Himank <himankbajaj10@gmail.com>
2022-01-22 15:13:51 +0530

    First Commit: Create the directory and setup the express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+ 
+app.listen(port, function(err){
+    if(err){
+        console.log(`Erro in running the server: ${err}`); //interpolation
+    }
+    console.log(`Server is running on port: ${port}`);
+});
